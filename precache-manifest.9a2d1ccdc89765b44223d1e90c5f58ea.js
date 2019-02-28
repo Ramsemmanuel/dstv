b015220e8e0968e599ea8560a08c6f8cbd4566ce
emmanuel.ramanyimi <emmanuel.ramanyimi@xisystems.co.za>
2019-02-28 13:57:36 +0200

    Updates

---
+++ precache-manifest.9a2d1ccdc89765b44223d1e90c5f58ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b1882970ad7a1313b3be",
+    "url": "/dstv/static/js/runtime~main.b1882970.js"
+  },
+  {
+    "revision": "434cce30cc628cd76949",
+    "url": "/dstv/static/js/main.434cce30.chunk.js"
+  },
+  {
+    "revision": "ff6c2caac9bcbd979b9a",
+    "url": "/dstv/static/js/2.ff6c2caa.chunk.js"
+  },
+  {
+    "revision": "434cce30cc628cd76949",
+    "url": "/dstv/static/css/main.d9d4a7eb.chunk.css"
+  },
+  {
+    "revision": "ff6c2caac9bcbd979b9a",
+    "url": "/dstv/static/css/2.56a58592.chunk.css"
+  },
+  {
+    "revision": "762f7b14e7da38d96876713004d4a55a",
+    "url": "/dstv/index.html"
+  }
+];
